f9f04950427dd686a2f229fbde00ddf0f6338cd1
Massimo Tascone <massimo@Massi.local>
2023-05-10 15:43:05 +0200

    search bar + testing with mock data

---
+++ src/components/Card.tsx
@@ -0,0 +1,12 @@
+export function Card(props) {
+  return (
+    <li>
+      <div>
+        <img src="" alt="" />
+      </div>
+      <div>
+        <p>Made by </p>
+      </div>
+    </li>
+  );
+}
